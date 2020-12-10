8e2c369e911181aeffe6236505ecf97d7bf1ac86
Devid <70418160+tuxcatenate@users.noreply.github.com>
2020-12-10 09:28:13 +0700

    pertemuan JWT + Node
    penambahan pertemuan 12

---
+++ Pertemuan 12 - PassportJS/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { ensureAuthenticated } = require('../config/auth');
+const router =  express.Router();
+
+
+router.get('/',(req,res)=>res.render('homepage'));
+
+//dashboard
+router.get("/dashboard", ensureAuthenticated, (req, res) =>
+    res.render("dashboard", {
+        name: req.user.name,
+    })
+);
+
+module.exports = router;
